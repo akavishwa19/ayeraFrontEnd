576f2ee9cddeb2f4fc9fe5dc618f12bd9a14f4a7
Akash <akash19102001@gmail.com>
2024-08-22 18:56:26 +0530

    final build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseurl: 'api.aayrajewellery.com',
+    baseurl: 'https://api.aayrajewellery.com/api/v1',
     imageUrl:'https://firebasestorage.googleapis.com/v0/b/aayra-c0699.appspot.com/o/',
     imageMetaUrl:'?alt=media',
     editorKey:'akt9dfsne5z1bf3u31mlxndjexhrnngj4fw3wi7fytl5gmng'
