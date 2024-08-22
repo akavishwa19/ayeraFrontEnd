f2dacddb7d3e4dd498c882317fdd2ab273da31d4
Akash <akash19102001@gmail.com>
2024-08-22 18:41:41 +0530

    environment variables

---
+++ src/environments/envireonment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    baseurl: 'api.aayrajewellery.com',
+    imageUrl:'https://firebasestorage.googleapis.com/v0/b/aayra-c0699.appspot.com/o/',
+    imageMetaUrl:'?alt=media',
+    editorKey:'akt9dfsne5z1bf3u31mlxndjexhrnngj4fw3wi7fytl5gmng'
+  };
+  
