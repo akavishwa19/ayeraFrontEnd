0136dd0b6b0d3a3cc2c2b047596800110827ef8b
Akash <akash19102001@gmail.com>
2024-09-17 14:36:41 +0530

    somehow finished compare , lets not touch it again as it works

--- src/app/shared/compare-product/compare-product.component.ts
+++ src/app/shared/compare-product/compare-product.component.ts
@@ -1,16 +1,14 @@
-import {Component, EventEmitter, OnInit} from '@angular/core';
-import {ComparedProductModel} from "../../Models/ComparedProduct.model";
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { ComparedProductModel } from '../../Models/ComparedProduct.model';
 
 @Component({
   selector: 'app-compare-product',
   templateUrl: './compare-product.component.html',
-  styleUrl: './compare-product.component.scss'
+  styleUrl: './compare-product.component.scss',
 })
-export class CompareProductComponent implements OnInit{
-
-  renderedArray:ComparedProductModel[]=[];
-
-
+export class CompareProductComponent implements OnInit {
+  renderedArray: any[] = [];
+  productRating:any[]=[]
 
   ngOnInit() {
     // this.renderedArray[1]={
@@ -28,10 +26,18 @@ export class CompareProductComponent implements OnInit{
     //   name:'',
     //   strikedPrice:0
     // }
-
   }
 
-  handleChange(event:any,index:number){
-      this.renderedArray[index]=event
+  handleChange(event: any, index: number) {
+
+    this.productRating[index]=event.averageRating;
+    if(event.variants.length==0){
+      this.renderedArray[index] = event;
+    }
+    else{
+      this.renderedArray[index]=event.variants[0]
+    }
+
+
   }
 }
--- src/app/shared/product-in-comparison/product-in-comparison.component.ts
+++ src/app/shared/product-in-comparison/product-in-comparison.component.ts
@@ -17,6 +17,7 @@ import { environment } from '../../../environments/environment';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
+import { event } from 'jquery';
 
 @Component({
   selector: 'app-product-in-comparison',
@@ -26,8 +27,11 @@ import { MessageService } from 'primeng/api';
 export class ProductInComparisonComponent implements OnInit {
   categoryUrl = environment.baseurl + '/categories';
   tagUrl: string = environment.baseurl + '/tags';
+  productUrl:string=environment.baseurl+'/product';
+  imageUrl:string=environment.imageUrl;
+  imageMetaUrl:string=environment.imageMetaUrl;
 
-  @Input() productData: ComparedProductModel = {
+  @Input() productData: any = {
     price: 0,
     color: '',
     size: '',
@@ -42,18 +46,39 @@ export class ProductInComparisonComponent implements OnInit {
     name: '',
     strikedPrice: 0,
   };
+  activeProduct:{
+    featuredImage:String,
+    name:String,
+    productCode:String,
+    averageRating,
+    price:Number,
+    sellingPrice:Number,
+    variants:[
+      {
+        images:[
+          String
+
+        ]
+      }
+   
+    ]
+
+  } | null=null
 
   @Output() productChange: EventEmitter<Object> = new EventEmitter<Object>();
 
   private modalService = inject(NgbModal);
   private modalRef: NgbModalRef;
   closeResult = '';
+  count:number=0
   primaryCategoryList = [];
   secondaryCategoryList = [];
   tertiaryCategoryList = [];
   patternList:any[]=[];
   quaternaryCategoryList = [];
   tagList = [];
+  searchTerm:string='';
+  searchResponse:any[]=[];
 
   product1: ComparedProductModel = {
     price: 2999,
@@ -71,46 +96,11 @@ export class ProductInComparisonComponent implements OnInit {
     strikedPrice: 3999,
   };
 
-  product2: ComparedProductModel = {
-    price: 5999,
-    color: 'Red',
-    size: 'L',
-    weight: 200,
-    length: 30,
-    breadth: 20,
-    stoneSize: 'Medium',
-    discount: 10,
-    image: 'assets/images/cardImage.png',
-    productCode: 'P12345',
-    rating: 4,
-    name: 'Sample Product2',
-    strikedPrice: 3999,
-  };
+  selectedProduct:any={
 
-  product3: ComparedProductModel = {
-    price: 5999,
-    color: 'Red',
-    size: 'L',
-    weight: 200,
-    length: 30,
-    breadth: 20,
-    stoneSize: 'Medium',
-    discount: 10,
-    image: 'assets/images/cardImage.png',
-    productCode: 'P12345',
-    rating: 4,
-    name: 'Sample Product3',
-    strikedPrice: 3999,
-  };
+  }
 
-  form:FormGroup=this.fb.group({
-    primary:new FormControl([]),
-    secondary:new FormControl([]),
-    tertiary:new FormControl([]),
-    quaternary:new FormControl([]),
-    tags:new FormControl([]),
-    productCode:new FormControl([]),
-  })
+
 
   constructor(
     private fb: FormBuilder,
@@ -152,68 +142,23 @@ export class ProductInComparisonComponent implements OnInit {
     }
   }
 
-  addType(num: number) {
+  addType(item:any) {
     this.modalService.dismissAll();
-    if (num === 0) {
-      this.productData = this.product1;
-      this.productChange.emit(this.product1);
-    } else if (num === 1) {
-      this.productData = this.product2;
-      this.productChange.emit(this.product2);
-    } else {
-      this.productData = this.product3;
-      this.productChange.emit(this.product3);
+    console.log(item)
+    if(item.variants.length==0){
+      this.activeProduct =item;
     }
+    else{
+      this.activeProduct=item.variants[0]
+    }
+    this.productData =item;
+      this.productChange.emit(this.productData);
   }
 
-  // getPrimaryCategories() {
-  //   this.http
-  //     .get(this.categoryUrl + '/primary-category')
-  //     .subscribe((res: any) => {
-  //       this.primaryCategoryList = res.data;
-  //     });
-  // }
-
-  // getSecondaryCategories() {
-  //   console.log(this.form.value);
-  //   this.http
-  //     .get(
-  //       this.categoryUrl +
-  //         '/secondary-category?id=' +
-  //         this.form.controls['primary'].value
-  //     )
-  //     .subscribe((res: any) => {
-  //       this.secondaryCategoryList = res.data;
-  //     });
-  // }
-
-  // getTertiaryCategories() {
-  //   this.http
-  //     .get(
-  //       this.categoryUrl +
-  //         '/tertiary-category?id=' +
-  //         this.form.controls['secondary'].value
-  //     )
-  //     .subscribe((res: any) => {
-  //       this.tertiaryCategoryList = res.data;
-  //     });
-  // }
-
-  // getQuaternaryCategories() {
-  //   this.http
-  //     .get(
-  //       this.categoryUrl +
-  //         '/quaternary-category?id=' +
-  //         this.form.controls['tertiary'].value
-  //     )
-  //     .subscribe((res: any) => {
-  //       this.quaternaryCategoryList = res.data;
-  //     });
-  // }
-
-  // getTags() {
-  //   this.http.get(this.tagUrl).subscribe((res: any) => {
-  //     this.tagList = res.data;
-  //   });
-  // }
+  fetchBySearchTerm(){
+    this.http.get(this.productUrl+'/by-search-term?searchTerm='+this.searchTerm).subscribe((res:any)=>{
+      this.searchResponse=res.data.data;
+      this.count=res.data.count;
+    })
+  }
 }
